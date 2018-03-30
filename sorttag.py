12234f7c82815462e349442e1b783eb998c88826
Xueyuan Jiang <alexxyjiang@gmail.com>
2018-03-30 16:45:48 +0800

    filter None values in seek

--- sorttag.py
+++ sorttag.py
@@ -9,7 +9,7 @@ def main():
       dtdict.add(name)
   for line in sys.stdin:
     names = line.strip("\n").split(" ")
-    dtags = [dtdict.seek(name) for name in names]
+    dtags = [dtdict.seek(name) for name in names if dtdict.seek(name) != None]
     dtags.sort()
     print(' '.join([str(tag) for tag in dtags]))
 
