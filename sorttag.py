435f0f3d16f40828e3d6e88a0902577c76026e20
Xueyuan Jiang <alexxyjiang@gmail.com>
2018-11-22 09:11:44 +0800

    version 0.4.0 - deduplicate tags with dict update

--- sorttag.py
+++ sorttag.py
@@ -10,6 +10,7 @@ def main():
   for line in sys.stdin:
     names = line.strip("\n").split(" ")
     dtags = [dtdict.seek(name) for name in names if dtdict.seek(name) != None]
+    dtags = list(set(dtags))
     dtags.sort()
     print(' '.join([str(tag) for tag in dtags]))
 
