28432d22adb020775d534076378e2d7b5f7cd083
Xueyuan Jiang <alexxyjiang@gmail.com>
2023-02-28 10:07:49 +0800

    support dump to md for douban tag dict, and format code

--- libdtag.py
+++ libdtag.py
@@ -1,63 +1,76 @@
 class DoubanTagDict(object):
-  def __init__(self):
-    self.__size__ = 0
-    self.__rank_dict__ = {}
-    self.__name_dict__ = {}
 
-  def __str__(self):
-    return 'size: %d'%(self.__size__)
+    def __init__(self):
+        self.__size__ = 0
+        self.__rank_dict__ = {}
+        self.__name_dict__ = {}
 
-  def add(self, name):
-    if self.seek(name) != None:
-      pass
-    else:
-      dtag = DoubanTag()
-      dtag.set(self.__size__, name)
-      self.__rank_dict__[self.__size__] = dtag
-      self.__name_dict__[name] = dtag
-      self.__size__ += 1
+    def __str__(self):
+        return 'size: %d' % (self.__size__)
 
-  def seek(self, key):
-    if isinstance(key, int):
-      if key in self.__rank_dict__:
-        return self.__rank_dict__[key]
-      else:
-        return None
-    else:
-      if key in self.__name_dict__:
-        return self.__name_dict__[key]
-      else:
-        return None
+    def add(self, name):
+        if self.seek(name) != None:
+            pass
+        else:
+            dtag = DoubanTag()
+            dtag.set(self.__size__, name)
+            self.__rank_dict__[self.__size__] = dtag
+            self.__name_dict__[name] = dtag
+            self.__size__ += 1
+
+    def seek(self, key):
+        if isinstance(key, int):
+            if key in self.__rank_dict__:
+                return self.__rank_dict__[key]
+            else:
+                return None
+        elif isinstance(key, str):
+            if key in self.__name_dict__:
+                return self.__name_dict__[key]
+            else:
+                return None
+        else:
+            return None
+
+    def write_md(self, file):
+        for i in range(self.__size__):
+            dtag = self.seek(i)
+            file.write(f'* {dtag}\n')
+
 
 class DoubanTag(object):
-  def __init__(self):
-    self.__rank__ = 0
-    self.__name__ = ''
 
-  def __str__(self):
-    return self.name()
+    def __init__(self):
+        self.__rank__ = 0
+        self.__name__ = ''
 
-  def __lt__(self, other):
-    if isinstance(other, DoubanTag):
-      return self.rank() < other.rank()
-    else:
-      return NotImplemented
+    def __str__(self):
+        return self.name()
 
-  def set(self, rank, name):
-    self.__rank__ = rank
-    self.__name__ = name
+    def __lt__(self, other):
+        if isinstance(other, DoubanTag):
+            return self.rank() < other.rank()
+        else:
+            return NotImplemented
 
-  def rank(self):
-    return self.__rank__
+    def set(self, rank, name):
+        self.__rank__ = rank
+        self.__name__ = name
 
-  def name(self):
-    return self.__name__
+    def rank(self):
+        return self.__rank__
+
+    def name(self):
+        return self.__name__
+
 
 # Global Access Part #
-VERSION = '0.1'
+VERSION = '0.2'
+
 
 def version():
-  print('libdtag version: ' + VERSION)
+    print('libdtag version: ' + VERSION)
+
 
 if __name__ == '__main__':
-  version()
+    version()
--- sorttag.py
+++ sorttag.py
@@ -1,18 +1,20 @@
 import sys
 import libdtag
 
+
 def main():
-  dtdict = libdtag.DoubanTagDict()
-  for line in open("./douban.md"):
-    if line.startswith("*"):
-      name = line.strip("\n")[2:]
-      dtdict.add(name)
-  for line in sys.stdin:
-    names = line.strip("\n").split(" ")
-    dtags = [dtdict.seek(name) for name in names if dtdict.seek(name) != None]
-    dtags = list(set(dtags))
-    dtags.sort()
-    print(' '.join([str(tag) for tag in dtags]))
+    dtdict = libdtag.DoubanTagDict()
+    for line in open('./douban.md'):
+        if line.startswith('*'):
+            name = line.strip('\n')[2 :]
+            dtdict.add(name)
+    for line in sys.stdin:
+        names = line.strip('\n').split(' ')
+        dtags = [dtdict.seek(name) for name in names if dtdict.seek(name) != None]
+        dtags = list(set(dtags))
+        dtags.sort()
+        print(' '.join([str(tag) for tag in dtags]))
+
 
 if __name__ == '__main__':
-  main()
+    main()
